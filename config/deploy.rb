4cb71d4c4f2eeeb235a6119eca45c8fa7e08b907
El Abdulla Karim <ghetto-workout-ru@yandex.ru>
2015-09-23 23:43:49 +0300

    ok

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :deploy_via, :copy
 
 # Define server(s)
 set :linked_files, fetch(:linked_files, []).
-  push('config/database.example.yml', 'config/secrets.yml')
+  push('config/database.yml', 'config/secrets.yml')
 set :linked_dirs, fetch(:linked_dirs, []).
   push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets')
 # SSH Options
