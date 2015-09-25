66b040baa3b5a44b6aa6af838b7e62576eb29720
El Abdulla Karim <ghetto-workout-ru@yandex.ru>
2015-09-25 19:21:55 +0300

    'restart'

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -21,10 +21,3 @@ timeout 30
 
 # Set up socket location
 listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
-
-# Logging
-stderr_path "#{shared_dir}/log/unicorn.stderr.log"
-stdout_path "#{shared_dir}/log/unicorn.stdout.log"
-
-# Set master PID location
-pid "#{shared_dir}/pids/unicorn.pid"
