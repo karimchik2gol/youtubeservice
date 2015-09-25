4d3af1dd84eef14e0f6673dcaf6aa5650cccc733
El Abdulla Karim <ghetto-workout-ru@yandex.ru>
2015-09-25 18:18:50 +0300

    'restart'

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,7 @@ module Youtubeservice
     config.assets.initialize_on_precompile = false
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-
+    config.assets.precompile += ['960sm.css']
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
 
