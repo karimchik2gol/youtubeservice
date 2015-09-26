cfc4491c5da5ccbeb38e4250a14d3b4e65751f5d
El Abdulla Karim <ghetto-workout-ru@yandex.ru>
2015-09-26 18:15:08 +0300

    ok

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,16 +1,6 @@
-root="/home/deployer/apps/blog/current"
-working_directory root
-pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
-
-listen "/tmp/unicorn.blog.sock"
-worker_processes 2
-timeout 30
-
 # set path to application
-app_dir = "/home/deployer/apps/blog/current"
-shared_dir = "/home/deployer/apps/blog/shared"
+app_dir="/home/deployer/apps/blog/"
+shared_dir = "#{app_dir}/shared"
 working_directory app_dir
 
 
@@ -20,4 +10,11 @@ preload_app true
 timeout 30
 
 # Set up socket location
-listen "#{shared_dir}/tmp/unicorn.blog.sock"
+listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+
+# Logging
+stderr_path "#{shared_dir}/log/unicorn.stderr.log"
+stdout_path "#{shared_dir}/log/unicorn.stdout.log"
+
+# Set master PID location
+pid "#{shared_dir}/pids/unicorn.pid"
