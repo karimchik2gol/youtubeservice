c8d6005c1d923345652b324ad6dd5845c33964af
El Abdulla Karim <ghetto-workout-ru@yandex.ru>
2015-09-27 21:34:29 +0300

    ok

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,37 @@
+# config valid only for current version of Capistrano
+lock '3.4.0'
+# Define the name of the application
+
+server '45.79.176.251', user: 'deployer', roles: %w{web app db}, :primary => true
+
+# Define where can Capistrano access the source repository
+# set :repo_url, 'https://github.com/[user name]/[application name].git'
 set :application, 'blog'
-set :repo_url, 'git@github.com:karimchik2gol/youtubeservice.git'
+#set :pty, true
+set :deploy_to, "/home/deployer/apps/blog"
+#set :deploy_via, :remote_cache
+#set :use_sudo, false
 
-set :deploy_to, '/home/deployer/apps'
+set :scm, :git
+set :scm_passphrase, "Youtubechannel2015"
+#set :branch, "master"
+set :repo_url, 'https://github.com/karimchik2gol/youtubeservice'
 
-set :linked_files, %w{config/database.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :user, "deployer"
+set :rails_env, "production"
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets}
 
+
+# SSH Options
+# See the example commented out section in the file
+# for more options.
 set :ssh_options, {
     forward_agent: true,
     auth_methods: %w(password),
     password: 'Youtubechannel2015',
     user: 'deployer',
 }
+
 
 namespace :deploy do
 
